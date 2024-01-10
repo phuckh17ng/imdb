ca5e9a51ed0928573fb666790e33be2477e56012
Khang <khanglmp@nec.vn>
2024-01-10 11:19:37 +0700

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import SliderBannerMovies from "../components/bannerMovies/SliderBannerMovies";
+import SliderFanFavoriteMovies from "../components/slider/sliderFanFavoriteMovies/SliderFanFavoriteMovies";
+import SliderTopMovies from "../components/slider/sliderTopMovies/SliderTopMovies";
+import SliderWatchlistMovies from "../components/slider/sliderWatchlistMovies/SliderWatchlistMovies";
+
+const HomePage = () => {
+  return (
+    <div className="w-full bg-black">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        limit={1}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <SliderBannerMovies />
+      <SliderTopMovies />
+      <SliderWatchlistMovies />
+      <SliderFanFavoriteMovies />
+    </div>
+  );
+};
+
+export default HomePage;
